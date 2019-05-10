c0983e184ee998db7fbb8a1fa133f4332f80e3a4
jondubere <heyjoeburn@gmail.com>
2019-05-10 17:17:43 +0100

    Started matching logic of GM lib to what is in Spotify

--- gmspotify.py
+++ gmspotify.py
@@ -1,4 +1,5 @@
 import gmusic
+import spotify
 import argparse
 
 {
@@ -13,6 +14,7 @@ albums = {}
 #albums = {'Bi5vplouym4ugzrtyyx7c7jh5fa': {'title': 'Mono No Aware', 'albumArtist': 'Various Artists', 'year': 2017, 'tracks': [{'title': 'VXOMEG', 'artist': 'Bill Kouligas', 'rating': ''}, {'title': 'Justforu', 'artist': 'Mya Gomez', 'rating': ''}, {'title': 'Stretch Deep (feat. Eve Essex)', 'artist': 'James K', 'rating': ''}, {'title': 'Heretic', 'artist': 'Oli XL', 'rating': ''}, {'title': 'Lugere', 'artist': 'Flora Yin-Wong', 'rating': ''}, {'title': 'Exasthrus (Pane)', 'artist': 'M.E.S.H.', 'rating': ''}, {'title': 'C6 81 56 28 09 34 31 D2 F9 9C D6 BD 92 ED FC 6F 6C A9 D4 88 95 8C 53 B4 55 DF 38 C4 AB E7 72 13', 'artist': 'TCF', 'rating': ''}, {'title': 'Ok, American Medium', 'artist': 'Jeff Witscher', 'rating': ''}, {'title': 'Open Invitation', 'artist': 'ADR', 'rating': ''}, {'title': 'Huit', 'artist': 'SKY H1', 'rating': ''}, {'title': 'Held', 'artist': 'Malibu', 'rating': ''}, {'title': 'Second Mistake', 'artist': 'AYYA', 'rating': ''}, {'title': 'Fr3sh', 'artist': 'Kareem Lotfy', 'rating': ''}, {'title': 'Eliminator', 'artist': 'Helm', 'rating': ''}, {'title': 'Limerence', 'artist': 'Yves Tumor', 'rating': '5'}, {'title': 'Zhao Hua', 'artist': 'HVAD & Pan Daijing', 'rating': '5'}]}}
 
 def parse_lib(lib):
+    albums = {}
     for track in lib:
         albumId = track['albumId']
         if albumId not in albums:
@@ -24,15 +26,60 @@ def parse_lib(lib):
             albums[albumId] = album
         album = albums[albumId]
         track_rating = track['rating'] if 'rating' in track else ''
+        # TODO should be mapped by track number?
         album['tracks'].append({
             'title': track['title'],
             'artist': track['artist'],
             'rating': track_rating
         })
+    return albums
 
 
+def _match_tracks(gm_tracks, s_tracks):
+    s_tracks_added = []
+    for gm_track in gm_tracks:
+        for s_track in s_tracks:
+            s_track_id = s_track['id']
+            if s_track_id in s_tracks_added:
+                continue
+            if gm_track['title'] == s_track['name'] and \
+                    gm_track['artist'] == s_track['artists'][0]['name']:
+                gm_track['spotifyId'] = s_track_id
+                s_tracks_added.append(s_track_id)
 
-def match(lib):
+
+def match(albums):
+    for albumId, album in albums.items():
+        album_title = album['title']
+        album_artist = album['albumArtist']
+        print('Processing {} - {}'.format(album_title, album_artist))
+        results = spotify.query_album_by_artist(album_title, album_artist)
+        num_results = results['albums']['total']
+        if num_results == 1:
+            spotify_album = results['albums']['items'][0]
+            album['spotifyId'] = spotify_album['id']
+            if len(album['tracks']) == spotify_album['total_tracks']:
+                print('Complete album added')
+                album['wholeAlbum'] = True
+            else:
+                print('Album partially added to library')
+                album['wholeAlbum'] = False
+            # Look up each track to get its ID
+            s_album_results = spotify.get_album_by_id(album['spotifyId'])
+            _match_tracks(album['tracks'], s_album_results['tracks']['items'])
+                
+
+                
+
+                
+        elif num_results > 1:
+            print('Could not accurately look up: {} - {}'.format(album_title,album_artist))
+        else:
+            print('No results found: {} - {}'.format(album_title,album_artist))
+
+
+            
+
 
 
 
@@ -42,7 +89,13 @@ def main():
     # Get all songs
     gm_lib = gm_api.get_all_songs()
     gmusic.gen_report(gm_lib)
-    match(gmusic.filter_added_tracks(gm_lib))
+    # Match with Spotify
+    albums = parse_lib(gm_lib)
+    match(albums)
+
+    # Generate report on what could be matched and what couldn't
+
+    # Ask user what they would like added
     
 
 
--- spotify.py
+++ spotify.py
@@ -2,7 +2,7 @@ import config
 import spotipy
 import spotipy.util as util
 
-_SP_REDIRECT_URL = 'http://localhost'
+_SP_REDIRECT_URL = 'http://localhost:8888/callback/'
 _SP_SCOPE = 'user-library-modify'
 
 
@@ -16,5 +16,16 @@ def get_sp_api():
         redirect_uri=_SP_REDIRECT_URL
         )
     return spotipy.Spotify(auth=token)
+
+
+def get_album_by_id(sp_api, id):
+    return sp_api.api.album(id)
+
+
+def query_album_by_artist(sp_api, album_title, album_artist):
+    query = 'album:{},artist:{}'.format(album_title, album_artist)
+    return sp_api.search(q=query, type='album')
+
+
 
 
